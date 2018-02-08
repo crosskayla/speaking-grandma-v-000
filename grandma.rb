1f2d9b2c8d68e1517bfcb209ea9653163736b6c8
Kayla Cross <kac288@georgetown.edu>
2018-02-08 18:04:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elif phrase = phrase.upcase
+  elsif phrase = phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
     
